d2659d98af26b474b13c14acf633958e2290d9f0
MohammedShams <mohdshams1802@gmail.com>
2025-09-16 21:09:04 +0530

    `Refactor onboarding page: add AppShell and update imports`

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,51 +1,61 @@
 "use client";
 
 import React, { useState } from "react";
-import { Stepper, Paper, Title, Text, Stack } from "@mantine/core";
+import { Stepper, Title, Text, Stack, AppShell } from "@mantine/core";
 
 const Onboarding = () => {
   const [active, setActive] = useState(0);
 
   return (
-    <Paper>
-      <Stack>
-        <Title size={40} c="dark">
-          Account Registration
-        </Title>
-        <Text c="gray">
-          Complete the following steps to create your account
-        </Text>
-      </Stack>
+    <AppShell header={{ height: 60, offset: false }} padding="md">
+      <AppShell.Header p="md"></AppShell.Header>
 
-      <Stepper
-        active={active}
-        onStepClick={setActive}
-        mt={30}
-        orientation="vertical"
-      >
-        <Stepper.Step label="First step" description="Customer Verification">
-          Step 1 content: Create an account
-        </Stepper.Step>
-        <Stepper.Step label="Second step" description="Account Type">
-          Step 2 content: Verify email
-        </Stepper.Step>
-        <Stepper.Step label="Third step" description="Brokerage Account Type">
-          Step 3 content: Get full access
-        </Stepper.Step>
-        <Stepper.Step label="Fourth step" description="Retirement Account Type">
-          Step 4 content: Get full access
-        </Stepper.Step>
-        <Stepper.Step label="Fifth step" description="Personal Information">
-          Step 5 content: Get full access
-        </Stepper.Step>
-        <Stepper.Step label="Six step" description="Joint Account Information">
-          Step 6 content: Get full access
-        </Stepper.Step>
-        <Stepper.Step label="Seventh step" description="Entity Information">
-          Step 7 content: Get full access
-        </Stepper.Step>
-      </Stepper>
-    </Paper>
+      <AppShell.Main pt="var(--app-shell-header-height)">
+        <Stack>
+          <Title size={40} c="dark">
+            Account Registration
+          </Title>
+          <Text c="gray">
+            Complete the following steps to create your account
+          </Text>
+        </Stack>
+
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          mt={30}
+          orientation="vertical"
+        >
+          <Stepper.Step label="First step" description="Customer Verification">
+            Step 1 content: Create an account
+          </Stepper.Step>
+          <Stepper.Step label="Second step" description="Account Type">
+            Step 2 content: Verify email
+          </Stepper.Step>
+          <Stepper.Step label="Third step" description="Brokerage Account Type">
+            Step 3 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step
+            label="Fourth step"
+            description="Retirement Account Type"
+          >
+            Step 4 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step label="Fifth step" description="Personal Information">
+            Step 5 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step
+            label="Six step"
+            description="Joint Account Information"
+          >
+            Step 6 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step label="Seventh step" description="Entity Information">
+            Step 7 content: Get full access
+          </Stepper.Step>
+        </Stepper>
+      </AppShell.Main>
+    </AppShell>
   );
 };
 
