4a7401b9b61262e9fd91626094bbbc2d4a95be81
MohammedShams <mohdshams1802@gmail.com>
2025-09-19 14:03:45 +0530

    layout for trade

---
+++ src/app/trade/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+import {
+  AppShell,
+  useMantineTheme,
+  useMantineColorScheme,
+} from "@mantine/core";
+import Header from "../components/Header/Header";
+import DoubleFooter from "../components/DoubleFooter.tsx/DoubleFooter";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  const theme = useMantineTheme();
+  const { colorScheme } = useMantineColorScheme();
+
+  return (
+    <AppShell header={{ height: 60 }} padding="md">
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+
+      <AppShell.Main
+        style={{
+          backgroundColor:
+            colorScheme === "dark"
+              ? theme.colors.dark[7]
+              : theme.colors.gray[2],
+        }}
+      >
+        {children}
+      </AppShell.Main>
+      <AppShell.Footer>
+        <DoubleFooter />
+      </AppShell.Footer>
+    </AppShell>
+  );
+}
